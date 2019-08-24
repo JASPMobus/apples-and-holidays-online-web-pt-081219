10692c62f6a6e842dc7719502bb955f0e2028488
James Mobus <mobusj@live.com>
2019-08-24 23:56:19 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,7 +92,7 @@ def all_supplies_in_holidays(holiday_hash)
   
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s}:"
-    " #{holiday.to_s}: #{holiday_hash[season][holiday].join(", ")}"
+    puts " #{holiday.to_s}: #{holiday_hash[season][holiday].join(", ")}"
     end
   end
 end
